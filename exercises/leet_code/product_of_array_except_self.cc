b7ac926af776bd3cf4a243acea114b4d7c850649
Bruno Alves <balves16@hotmail.com>
2023-07-17 00:12:36 +0200

    constant space complexity

--- exercises/leet_code/product_of_array_except_self.cc
+++ exercises/leet_code/product_of_array_except_self.cc
@@ -4,6 +4,8 @@ using namespace std;
 
 // 1. Must run in O(n)
 // 2. Cannot use the division operation
+
+// O(n) time, O(n) space
 vector<int> productExceptSelf(vector<int>& nums) {
   vector<int> res(nums.size());
   vector<int> left(nums.size(), 1.);
@@ -16,6 +18,20 @@ vector<int> productExceptSelf(vector<int>& nums) {
   }
   for(int i=0; i<nums.size(); ++i) {
 	res[i] = left[i] * right[i];
+  }
+  return res;
+}
+
+// O(n) time, O(1) space
+vector<int> productExceptSelf2(vector<int>& nums) {
+  vector<int> res(nums.size(), 1);
+  for(int i=1; i<nums.size(); ++i) {
+	res[i] = res[i-1] * nums[i-1];
+  }
+  int right = 1;
+  for (int i=nums.size()-1; i>=0; --i) {
+	res[i] *= right;
+	right *= nums[i];
   }
   return res;
 }
@@ -35,5 +51,11 @@ int main(int argc, char** argv)
   vector<int> res2 = productExceptSelf(prompt2);
   printArray(res1);
   printArray(res2);
+
+  vector<int> res3 = productExceptSelf2(prompt1);
+  vector<int> res4 = productExceptSelf2(prompt2);
+  printArray(res3);
+  printArray(res4);
+
   return 0;
 }
