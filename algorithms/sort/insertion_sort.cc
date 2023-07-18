7606fdefe6585501b4dc5419f9ee5b4f4676b436
Bruno Alves <balves16@hotmail.com>
2023-07-19 00:45:29 +0200

    passing 159/190 cases

--- algorithms/sort/insertion_sort.cc
+++ algorithms/sort/insertion_sort.cc
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 
 template <typename T>
@@ -15,11 +16,22 @@ void insertionSort(vector<T>& v) {
   }
 }
 
+template <typename T>
+void insertionSortSwap(vector<T>& v) {
+  for(int i=1; i<v.size(); ++i) {
+	int j = i-1;
+	while(j>=0 and v[j+1] < v[j]) {
+	  std::swap(v[j+1], v[j]);
+	  --j;
+	}
+  }
+}
+
 int main(int argc, char *argv[])
 {
   //std::vector<float> vec = {1.4, 6.8, 3.4, 1.0, 7.9, -1.4};
   std::vector<int> vec = {5, 2, 4, 6, 1, 3};
-  insertionSort(vec);
+  insertionSortSwap(vec);
 
   for(auto &v: vec) {
 	std::cout << v << " ";
